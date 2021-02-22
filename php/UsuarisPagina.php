d5035adfb01ed32c0b655aae94999fceccb70320
mbusquets <mbusquets11372@alumnes.iesmanacor.cat>
2021-02-22 13:38:48 +0100

    22-02-2021-Pàgina d'usuaris

---
+++ php/UsuarisPagina.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="shortcut icon" href="imatges/logoicon.ico">
+  <title>Nima Deports</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" type="text/css" href="..\css\CssUsuarisPagina.css">
+</head>
+<body>
+
+<div style="padding:15px;text-align:center;">
+  <img src="../imatges/logo1.png" width="200px"><h1 style="color:white;"><span class="linia">Descobreix els nostres productes</span></h1>
+</div>
+
+<div style="overflow:auto;">
+  <div class="menu">
+    <!--POSAR FORMULARI AMB IMATGES MIAM QUE TAL-->
+        <img src="../imatges/pantalones.jpg" width="10%" style="border-radius:50px;border: 1px solid black;">
+        <img src="../imatges/pantalones.jpg" width="10%" style="border-radius:50px;border: 1px solid black;">
+        <img src="../imatges/pantalones.jpg" width="10%" style="border-radius:50px;border: 1px solid black;">
+        <img src="../imatges/pantalones.jpg" width="10%" style="border-radius:50px;border: 1px solid black;">
+        <img src="../imatges/pantalones.jpg" width="10%" style="border-radius:50px;border: 1px solid black;">
+  </div>
+
+  <div class="main">
+    <h2>Lorum Ipsum</h2>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+    <img src="../imatges/logo1.png" width="50%"><img src="../imatges/logo1.png" width="50%">
+  </div>
+</div>
+
+<div class="footer">© 2020 - 2021 - NIMA, SL</div>
+
+</body>
+</html>
