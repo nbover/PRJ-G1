17ca37208494a99a920ba484f51515b60cdafeb2
mbusquets <mbusquets11372@alumnes.iesmanacor.cat>
2021-03-06 19:05:47 +0100

    06-03-2021

--- php/AdminPagina.php
+++ php/AdminPagina.php
@@ -326,7 +326,7 @@ if ($existent=='existeix') {
   <link rel="shortcut icon" href="../imatges/logoicon.ico">
   <title>Nima Deports</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="..\css\CssAdminPagina2.css">
+  <link rel="stylesheet" type="text/css" href="..\css\CssAdminPagina1.css">
 </head>
 <body>
 
